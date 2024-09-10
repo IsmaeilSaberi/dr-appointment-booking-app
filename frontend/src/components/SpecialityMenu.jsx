c046a520e7d2f6453479f14b35ea989b4118f5cb
Ismaeil Saberi <ismaeilsaberi@gmail.com>
2024-09-10 05:04:04 -0700

    adding Speciality menu

---
+++ frontend/src/components/SpecialityMenu.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { specialityData } from "../assets/assets";
+import { Link } from "react-router-dom";
+
+const SpecialityMenu = () => {
+  return (
+    <div
+      className="flex flex-col items-center gap-4 py-16 text-gray-800"
+      id="speciality"
+    >
+      <h1 className="text-3xl font-medium">Find by Speciality</h1>
+      <p className="sm:w-1/3 text-center text-sm">
+        Browse through our extensive list of trusted doctors, schedule your
+        appointment hassle-free.
+      </p>
+      <div className="flex sm:justify-center gap-4 pt-5 w-full overflow-scroll">
+        {specialityData.map((item, index) => (
+          <Link
+            className="flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500"
+            key={index}
+            to={`/doctors/${item.speciality}`}
+          >
+            <img className="w-16 sm:w-24 mb-2" src={item.image} alt="" />
+            <p>{item.speciality}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SpecialityMenu;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import SpecialityMenu from "../components/SpecialityMenu";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <SpecialityMenu />
     </div>
   );
 };
