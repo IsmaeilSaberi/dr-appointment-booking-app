b8e8896e2afcf7ebbaca141b12dcf76cddeff44f
Ismaeil Saberi <ismaeilsaberi@gmail.com>
2024-09-10 04:36:14 -0700

    header completed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -19,8 +19,12 @@ const Header = () => {
             schedule your appointment hassle-free.
           </p>
         </div>
-        <a href="">
-          Book appointment <img src={assets.arrow_icon} alt="" />
+        <a
+          href="#speciality"
+          className="flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300"
+        >
+          Book appointment{" "}
+          <img className="w-3" src={assets.arrow_icon} alt="" />
         </a>
       </div>
 
