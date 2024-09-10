3b745832493b12fb6663fa91007e848dfe63d4ae
Ismaeil Saberi <ismaeilsaberi@gmail.com>
2024-09-09 22:21:54 -0700

    adding some little structure to the header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Header = () => {
+  return (
+    <div className="flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20">
+      {/* Left Side */}
+      <div className="md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]">
+        <p className="text-3xl md:text-4xl lg:text-4xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight">
+          Book Appointment
+          <br />
+          With Trusted Doctors
+        </p>
+        <div className=" flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light">
+          <img className="w-28" src={assets.group_profiles} alt="" />
+          <p>
+            Simply browse through our extensive list of trusted doctors,{" "}
+            <br className="hidden sm:block" />
+            schedule your appointment hassle-free.
+          </p>
+        </div>
+        <a href="">
+          Book appointment <img src={assets.arrow_icon} alt="" />
+        </a>
+      </div>
+
+      {/* Right Side */}
+      <div className="md:w-1/2 relative">
+        <img
+          className="w-full md:absolute bottom-0 h-auto rounded-lg"
+          src={assets.header_img}
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../components/Header";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
