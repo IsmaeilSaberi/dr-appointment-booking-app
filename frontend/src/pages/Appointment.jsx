99adf69d9cba4fbe371c69069839fffbe849471d
Ismaeil Saberi <ismaeilsaberi@gmail.com>
2024-09-13 04:53:38 -0700

    completing some part of doctor details

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
 
 const Appointment = () => {
-  return <div>Appointment</div>;
+  const { docId } = useParams();
+
+  const { doctors } = useContext(AppContext);
+  const [docInfo, setDocInfo] = useState(null);
+
+  const fetchDocInfo = () => {
+    const docInfo = doctors.find((doc) => (doc._id = docId));
+    setDocInfo(docInfo);
+  };
+
+  useEffect(() => {
+    fetchDocInfo();
+  }, [doctors, docId]);
+
+  return (
+    docInfo && (
+      <div>
+        {/* Doctor Dtails */}
+        <div>
+          <div>
+            <img src={docInfo.image} alt="" />
+          </div>
+          <div>
+            {/* Doc info like name degree and .... */}
+            <p>
+              {docInfo.name} <img src={assets.verified_icon} alt="" />
+            </p>
+            <div>
+              <p>
+                {docInfo.degree} - {docInfo.speciality}
+              </p>
+              <button>{docInfo.experience}</button>
+            </div>
+            {/* Doctor About */}
+            <div>
+              <p>
+                About <img src={assets.info_icon} alt="" />
+              </p>
+              <p>{docInfo.about}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  );
 };
 
 export default Appointment;
