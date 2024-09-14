ab0592e64dd8282284d03b8d3586cee6aeed413f
Ismaeil Saberi <ismaeilsaberi@gmail.com>
2024-09-13 23:06:39 -0700

    completeing doctor details

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -4,8 +4,11 @@ import { doctors } from "../assets/assets";
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
+  const currencySymbol = "$";
+
   const value = {
     doctors,
+    currencySymbol,
   };
 
   return (
--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -6,7 +6,7 @@ import { assets } from "../assets/assets";
 const Appointment = () => {
   const { docId } = useParams();
 
-  const { doctors } = useContext(AppContext);
+  const { doctors, currencySymbol } = useContext(AppContext);
   const [docInfo, setDocInfo] = useState(null);
 
   const fetchDocInfo = () => {
@@ -21,29 +21,45 @@ const Appointment = () => {
   return (
     docInfo && (
       <div>
-        {/* Doctor Dtails */}
-        <div>
+        {/* Doctor Details */}
+        <div className="flex flex-col sm:flex-row gap-4">
           <div>
-            <img src={docInfo.image} alt="" />
+            <img
+              className="bg-primary w-full sm:max-w-72 rounded-lg"
+              src={docInfo.image}
+              alt=""
+            />
           </div>
-          <div>
+          <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0">
             {/* Doc info like name degree and .... */}
-            <p>
-              {docInfo.name} <img src={assets.verified_icon} alt="" />
+            <p className="flex items-center gap-2 text-2xl font-medium text-gray-900">
+              {docInfo.name}{" "}
+              <img className="w-5" src={assets.verified_icon} alt="" />
             </p>
-            <div>
+            <div className="flex items-center gap-2 text-sm mt-1 text-gray-600">
               <p>
                 {docInfo.degree} - {docInfo.speciality}
               </p>
-              <button>{docInfo.experience}</button>
+              <button className="py-0.5 px-2 border text-xs rounded-full">
+                {docInfo.experience}
+              </button>
             </div>
             {/* Doctor About */}
             <div>
-              <p>
+              <p className="flex items-center gap-1 text-sm font-medium text-gray-900 mt-3">
                 About <img src={assets.info_icon} alt="" />
               </p>
-              <p>{docInfo.about}</p>
+              <p className="text-sm text-gray-500 max-w-[700px] mt-1">
+                {docInfo.about}
+              </p>
             </div>
+            <p className="text-gray-500 font-medium mt-4">
+              Appointment fee:{" "}
+              <span className="text-gray-600">
+                {currencySymbol}
+                {docInfo.fees}
+              </span>
+            </p>
           </div>
         </div>
       </div>
