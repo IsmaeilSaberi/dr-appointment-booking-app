6a548cdc089aff190a80a6ef9345eabc7b4aad7e
Ismaeil Saberi <ismaeilsaberi@gmail.com>
2024-09-15 12:44:49 -0700

    creating doctorModel

--- backend/models/doctorModel.js
+++ backend/models/doctorModel.js
@@ -2,4 +2,17 @@ import mongoose from "mongoose";
 
 const doctorSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  about: { type: String, required: true },
+  fees: { type: Number, required: true },
+  image: { type: Array, required: true },
+  speciality: { type: String, required: true },
+  experience: { type: String, required: true },
+  degree: { type: String, required: true },
+  date: { type: Number, required: true },
+  address: { type: Object, required: true },
 });
+
+const doctorModel =
+  mongoose.models.doctor || mongoose.model("doctor", doctorSchema);
+
+export default doctorModel;
