723166a811a23afacdd863b69ae800fbe69696a2
Ismaeil Saberi <ismaeilsaberi@gmail.com>
2024-09-16 22:32:06 -0700

    adding admin login function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -75,6 +75,22 @@ const registerUser = async (req, res) => {
 };
 
 // Route for admin login
-const adminLogin = async (req, res) => {};
+const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export { loginUser, registerUser, adminLogin };
