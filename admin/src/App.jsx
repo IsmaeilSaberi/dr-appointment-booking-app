0b794c0cdafce0d49105295b04292a6dda077308
Ismaeil Saberi <ismaeilsaberi@gmail.com>
2024-09-21 20:30:37 -0700

    completing admin pannel logigs and routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
+import Login from "./components/Login";
 
 const App = () => {
+  const [token, setToken] = useState("11");
+
   return (
     <div className="bg-gray-50 min-h-screen">
-      <>
-        <Navbar />
-        <hr />
-        <div className="flex w-full">
-          {" "}
-          <Sidebar />
-        </div>
-      </>
+      {token === "" ? (
+        <Login />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            {" "}
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
